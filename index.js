3c33161a8ce122ffa5c9a765f1cb6e1ccde6331f
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-10-27 08:23:59 +0530

    added graphql intraction

--- index.js
+++ index.js
@@ -16,16 +16,88 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var apiRouters = require('./apiRouters');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/tr1', function(req, res){
+  var dname = 'acidity';
+  var query=`query{
+	symptom(where:{
+    disease:{
+      _like:"${dname}"
+    }
+  }){
+    remedies
+    requirements
+  }
+}`
+  console.log("caught a disease");
+  var selectOptions = {
+  url: "https://infinitedebug.herokuapp.com/v1alpha1/graphql",
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Hasura-Access-Key': 'coolhack'
+  },
+  body: JSON.stringify({
+      query
+  })
+}
+request(selectOptions, function(error, response, body) {
+  if (error) {
+      console.log('Error from select request: ');
+      console.log(error)
+      res.status(500).json({
+        'error': error,
+        'message': 'Select request failed'
+      });
+  }
+  console.log(body);
+  var resy = JSON.parse(body);
+    res.send(resy.data.symptom[0].remedies);
+  })
+})
 app.post('/getdisease',function(req,res){
-  console.log(req.body);
-  res.end("yes");
+  if(req.body.queryResult.intent.displayName='GetDisease'){
+    var dname= req.body.queryResult.parameters.disease;
+    var query=`query{
+    symptom(where:{
+      disease:{
+        _like:"${dname}"
+      }
+    }){
+      remedies
+      requirements
+    }
+  }`
+    console.log("caught a disease");
+    var selectOptions = {
+    url: "https://infinitedebug.herokuapp.com/v1alpha1/graphql",
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Hasura-Access-Key': 'coolhack'
+    },
+    body: JSON.stringify({
+        query
+    })
+  }
+  request(selectOptions, function(error, response, body) {
+    if (error) {
+        console.log('Error from select request: ');
+        console.log(error)
+        res.status(500).json({
+          'error': error,
+          'message': 'Select request failed'
+        });
+    }
+    console.log(body);
+    var resy = JSON.parse(body);
+      res.send(resy.data.symptom[0].remedies);
+    })
+  }
 });
 
-app.use('/api', apiRouters);
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
